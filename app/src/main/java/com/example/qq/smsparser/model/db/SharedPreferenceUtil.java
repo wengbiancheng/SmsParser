f5de88f84d3f6e592782eae5b07fae073846a5dd
wengcanbiao <1569702426@qq.com>
2017-05-07 17:06:28 +0800

    服务器号码修正

--- app/src/main/java/com/example/qq/smsparser/model/db/SharedPreferenceUtil.java
+++ app/src/main/java/com/example/qq/smsparser/model/db/SharedPreferenceUtil.java
@@ -7,16 +7,16 @@ import android.content.SharedPreferences;
 public class SharedPreferenceUtil {
 
     public static void writeServerNumber(String content, Context context){
-        SharedPreferences sharedPreferences= context.getSharedPreferences("server",
-                Activity.MODE_PRIVATE);
+        SharedPreferences sharedPreferences= context.getApplicationContext().getSharedPreferences("server",
+                Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putString("number", content);
         editor.commit();
     }
 
     public static String getServerNumber(Context context){
-        SharedPreferences sharedPreferences= context.getSharedPreferences("test",
-                Activity.MODE_PRIVATE);
+        SharedPreferences sharedPreferences= context.getApplicationContext().getSharedPreferences("test",
+                Context.MODE_PRIVATE);
         String number=sharedPreferences.getString("number","106902208411010008");
         return number;
     }
